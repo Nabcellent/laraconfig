7411f58ec9a57cc673d9cd2ad04439d3e7a0d408
nabcellent <nabcellent.dev@gmail.com>
2024-07-24 15:25:22 +0300

    Upgrades entire package

--- tests/BaseTestCase.php
+++ tests/BaseTestCase.php
@@ -2,10 +2,8 @@
 
 namespace Tests;
 
-use Orchestra\Testbench\Attributes\WithMigration;
 use Orchestra\Testbench\TestCase;
 
-#[WithMigration]
 class BaseTestCase extends TestCase
 {
     use RegistersPackage;
